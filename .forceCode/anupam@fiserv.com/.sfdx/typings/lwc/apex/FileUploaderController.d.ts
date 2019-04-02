ee98ef40ff1903b982950c90334e90ece6e4b7c5
anupam081 <anup081@gmail.com>
2019-04-02 18:08:26 +0530

    staged

---
+++ .forceCode/anupam@fiserv.com/.sfdx/typings/lwc/apex/FileUploaderController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/FileUploaderController.insertFile" {
+  export default function insertFile(param: {recId: any, fileName: any, base64: any, objecttype: any}): Promise<any>;
+}
+declare module "@salesforce/apex/FileUploaderController.getImageUrlFromAttachment" {
+  export default function getImageUrlFromAttachment(param: {recId: any}): Promise<any>;
+}
---
+++ .forceCode/anupam@fiserv.com/.sfdx/typings/lwc/apex/UserUtilityApexController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/UserUtilityApexController.findUsers" {
+  export default function findUsers(param: {searchKey: any}): Promise<any>;
+}
+declare module "@salesforce/apex/UserUtilityApexController.loginhistory" {
+  export default function loginhistory(param: {userid: any}): Promise<any>;
+}
